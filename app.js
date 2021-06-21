6f9475f81416a8bf8c8499f0e971f1e3d953b913
Danilo Mendes <mmdanilo@icloud.com>
2021-06-21 16:44:04 +0100

    Solved Lab

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express(); 
+
+app.use(express.static("public")); 
+
+app.get("/home", (request, response) => {
+    response.sendFile(__dirname + "/views/home.html")
+});
+
+app.get("/about", (request, response) => {
+    response.sendFile(__dirname + "/views/about.html")
+});
+
+app.get("/works", (request, response) => {
+    response.sendFile(__dirname + "/views/works.html")
+});
+
+app.get("/gallery", (request, response) => {
+    response.sendFile(__dirname + "/views/gallery.html")
+});
+
+app.listen(4000, () => console.log("Server listening on port 4000"))
+
